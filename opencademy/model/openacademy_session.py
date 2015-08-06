54c089b677277df1146e148ca3302750858dab15
lihanha <llihan.hernan@gmail.com>
2015-08-06 16:58:39 +0000

    [IMP] openacademy: Adding states and buttons type object

--- opencademy/model/openacademy_session.py
+++ opencademy/model/openacademy_session.py
@@ -28,6 +28,12 @@ class Session(models.Model):
     attendees_count = fields.Integer(
         string="Attendees count", compute='_get_attendees_count', store=True)
     color = fields.Integer()
+    state = fields.Selection([
+                              ('draft', 'Draft'),
+                              ('confirmed', 'Confirmed'),
+                              ('done', 'Done'),
+                             ], default='draft', readonly=True)
+
  
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -94,5 +100,18 @@ class Session(models.Model):
     @api.depends('attendee_ids')
     def _get_attendees_count(self):
         self.attendees_count = len(self.attendee_ids)
+   
+    @api.one
+    def action_draft(self):
+        self.state = 'draft'
+    
+    @api.one
+    def action_confirm(self):
+        self.state = 'confirmed'
+
+    @api.one
+    def action_done(self):
+        self.state = 'done'
+
 
 # vim :expandtab:smart indent: tabstop=4:softtabstop=4:shifwidth=4;
